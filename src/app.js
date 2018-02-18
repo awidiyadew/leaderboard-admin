7739f35279206257f8809de55c9ec1349122c542
awidiyadew <awidiya.dewa@gmail.com>
2018-02-18 22:43:10 +0700

    change dotenv key name to caps

--- src/app.js
+++ src/app.js
@@ -5,13 +5,14 @@ dotenv.config();
 
 function init() {
   const config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
+
   firebase.initializeApp(config);
   const firestore = firebase.firestore();
   firestore
