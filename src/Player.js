ea5a03648f4d8b849f7fcc79ff0c6269df8fd063
awidiyadew <awidiya.dewa@gmail.com>
2018-02-18 23:51:47 +0700

    create Player model

---
+++ src/Player.js
@@ -0,0 +1,23 @@
+export default class Player {
+  constructor(nik, name, nationality, photo, score) {
+    this._nik = nik;
+    this._name = name;
+    this._nationality = nationality;
+    this._photo = photo;
+    this._score = score;
+  }
+
+  toObject() {
+    return {
+      nik: this._nik,
+      name: this._name,
+      nationality: this._nationality,
+      photo: this._photo,
+      score: this._score
+    }
+  }
+
+  getNik() {
+    return this._nik.toString();
+  }
+}
